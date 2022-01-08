7c34ad3c0dc832a39fc50557d78360ac52a76e52
Nico164 <nicolas.faite@gmail.com>
2022-01-08 11:51:33 +0700

    :fire:

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,41 +1,47 @@
-import React, { Fragment } from "react"
+import axios from "axios"
+import React, { Fragment, useEffect, useState } from "react"
 
 export function SlideShow() {
+    const [slides, setSlides] = useState([])
+    useEffect(() => {
+        getBanners()
+
+    }, [])
+
+    async function getBanners() {
+        const result = await axios.get('http://localhost:8080/banners')
+        console.log(result?.data.data)
+        setSlides(result?.data.data)
+    }
     return (
         <Fragment>
             <div
-            style= {{
-                height: 400
+                style={{
+                    height: 400
 
-            }}
-            id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                }}
+                id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
-                <div className="carousel-inner" style = {{height: 400}}>
-                    <div className="carousel-item active">
-                        <img style= {{height: 400, objectFit: 'cover'}} src="https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" className="d-block w-100" alt="..." />
+                <div className="carousel-inner" style={{ height: 400 }}>
+            {
+                slides.map((item, index) => (
+                    <div className={"carousel-item " + `${index === 0 ? 'active' : ''}`} 
+                    key={index}>
+                        <img style={{ height: 400, objectFit: 'cover' }} src={item?.images}
+                            className="d-block w-100" alt="..." />
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>First slide label</h5>
-                            <p>Some representative placeholder content for the first slide.</p>
+                            <h5>{item?.title}</h5>
+                            <p>{item?.description}</p>
                         </div>
                     </div>
-                    <div className="carousel-item">
-                        <img style= {{height: 400, objectFit: 'cover'}} src="https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Second slide label</h5>
-                            <p>Some representative placeholder content for the second slide.</p>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <img style= {{height: 400, objectFit: 'cover'}} src="https://images.unsplash.com/34/BA1yLjNnQCI1yisIZGEi_2013-07-16_1922_IMG_9873.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80" className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Third slide label</h5>
-                            <p>Some representative placeholder content for the third slide.</p>
-                        </div>
-                    </div>
+
+                ))
+            }
+
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
